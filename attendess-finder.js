0e2cfd2a4c2b21985102348182095a364fc0d049
patrickgiry <patrickgiry@gmail.com>
2025-08-02 00:12:15 +0200

    Closure - Avoid to pass result to addIf

--- attendess-finder.js
+++ attendess-finder.js
@@ -2,16 +2,17 @@ export function findByInfixOfFirstName(query, attendees) {
     const result = [];
 
     const predicate = (attendees, query) => matches(attendees, query);
+    const append = attendee => result.push(attendee);
 
     for (const attendee of attendees) {
-        addIfMatches(predicate, attendee, query, result);
+        addIfMatches(predicate, attendee, query, append);
     }
     return result;
 }
 
-function addIfMatches(predicate, attendee, query, result) {
+function addIfMatches(predicate, attendee, query, append) {
     if (predicate(attendee, query)) {
-        result.push(attendee);
+        append(attendee);
     }
 }
 
