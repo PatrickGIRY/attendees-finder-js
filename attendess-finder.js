88c01787ce6738de47563f24444198f8b4eeb882
patrickgiry <patrickgiry@gmail.com>
2025-08-01 23:43:15 +0200

    Suppress second level of indentation

--- attendess-finder.js
+++ attendess-finder.js
@@ -2,11 +2,15 @@ export function findByInfixOfFirstName(query, attendees) {
     const result = [];
 
     for (const attendee of attendees) {
-        if (matches(attendee, query)) {
-            result.push(attendee);
-        }
+        addIfMatches(attendee, query, result);
     }
     return result;
+}
+
+function addIfMatches(attendee, query, result) {
+    if (matches(attendee, query)) {
+        result.push(attendee);
+    }
 }
 
 function matches(attendee, query) {
