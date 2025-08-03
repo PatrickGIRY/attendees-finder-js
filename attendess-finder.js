94d04d4892826835b57967623fbc1a9c5a30b540
patrickgiry <patrickgiry@gmail.com>
2025-08-03 11:11:20 +0200

    Final version

--- attendess-finder.js
+++ attendess-finder.js
@@ -1,14 +1,11 @@
 export function findByInfixOfFirstName(query, attendees) {
     
-    const predicate = matches(query);
-    
-    return filter(predicate, attendees);
+    return filter(matches(query), attendees);
 }
 
 function filter(predicate, attendees) {
     return attendees.filter(predicate, attendees)
 }
-
 
 function matches(query) {
     return attendee => attendee.firstName.includes(query);
